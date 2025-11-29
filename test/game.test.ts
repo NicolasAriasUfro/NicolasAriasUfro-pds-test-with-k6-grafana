dd857d6bd864843f66423c114ab58e6305ee1e6f
Alex Sagredo Burgos <alexsagredo1204@gmail.com>
2025-11-29 14:14:36 -0300

    Adicion de test de carga

---
+++ test/game.test.ts
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { Options } from 'k6/options';
+
+export const options: Options = {
+  vus: 100,        
+  duration: '30s', 
+};
+
+export default function () {
+
+    http.get('http://localhost:3000/games')
+
+    const payload = JSON.stringify({
+        name: 'Galaga',
+        genre: 'action',
+        platform: 'PC',
+        releaseDate: '2025-01-01',
+        price: '3000',
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    http.post('http://localhost:3000/games', payload, params);
+
+    sleep(3);
+}
